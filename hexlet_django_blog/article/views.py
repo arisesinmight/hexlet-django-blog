895db261f0c505bfa952289e498c675da5b5d053
Pavel Ilchenko <arisesinmight@gmail.com>
2024-12-19 23:58:56 +0300

    add articles_index.html

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('article')
+    return render(request, 'article/articles_index.html', context={
+        'name': 'ARTICLE',
+    })
